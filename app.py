cb20e961cb2673c3a5c2ec21a73c3721f34919c1
Janakeerthana B <janakeerthana123@gmail.com>
2026-02-18 22:46:34 +0530

    Fix DB connection using env variables

--- app.py
+++ app.py
@@ -42,7 +42,14 @@ import os
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(os.environ.get("DATABASE_URL"))
+    return psycopg2.connect(
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT"),
+        sslmode="require"
+    )
 
 # ==========================================================
 # HEALTH CHECK
